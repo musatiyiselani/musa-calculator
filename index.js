cf5b9fde5f9b1e448863d33a4f20cc72a1ca6236
musatiyiselani <musatiyiselani89@gmail.com>
2026-07-17 03:11:18 +0200

    Enabled the on/off button.

--- index.js
+++ index.js
@@ -1,125 +1,158 @@
+// Working on the on/off button
+
+const on_off_btn = document.getElementById("on-Btn");
 const display = document.getElementById("display");
 
 let math_op = "";
+let isOn = false;
+
+
+
+on_off_btn.addEventListener('click',
+    function togglePower() {
+
+        isOn = !isOn;
+
+        if(isOn) {
+            display.value = '0';
+            math_op = "0"
+            on_off_btn.textContent = 'off';
+        }
+        else {
+            display.value = ''
+            math_op = "";
+            on_off_btn.textContent = 'on';
+        }
+})
+
+
+// Working on the display value
+
+
 
 function appendToDisplay(input) {
-    /* if(input != '.' && math_op === '0'){
-        math_op = input;
-        display.value = input;
-    }
-    else if(input === '÷'){
-        math_op += '/';
-        display.value += input;
-    }
-    else if(input === '^'){
-        math_op += '**';
-        display.value += input;
-    }
-    // π
-    else if(input === 'π'){
-        math_op += Math.PI;
-        display.value += input;
-    }
-    else if(input === '^2'){
-        math_op += '**2';
-        display.value += input;
-    }
-    else if (input === '×'){
-        math_op += '*';
-        display.value += input;
-    }
-    else{
-        math_op += input;
-        display.value += input;
-    }
-    */
 
-    if((input != '.' && input != '²') && math_op === '0'){
-        math_op = input;
-        display.value = input;
+    if(isOn === false) {
+       return;
     }
-    else {
-        math_op += input;
-        display.value += input;
+
+    else{ 
+        if((input != '.' && input != '²') && math_op === '0'){
+            math_op = input;
+            display.value = input;
+        }
+        else {
+            math_op += input;
+            display.value += input;
+        }
+
+        if(math_op === '') {
+            display.value = '0';
+            math_op = "0"
+        }
     }
     
 }
 
+// The open bracket
+
 
 function openBracket(input) {
-    if(math_op.length === 0 || '+*/-('.includes(math_op.at(-1))){
-        math_op += '(';
-        display.value += input;
+    if(isOn === false) {
+       return;
     }
-    else {
-        math_op += '*(';
-        display.value += input;
+    else{
+        if(math_op.length === 0 || '+*/-('.includes(math_op.at(-1))){
+            math_op += '(';
+            display.value += input;
+        }
+        else {
+            math_op += '*(';
+            display.value += input;
+        }
     }
 }
 
-
+// Working on the clear button
 
 function clearDisplay() {
-    math_op = "";
-    display.value = "";
+    if(isOn === false) {
+       return;
+    } else{
+        math_op = "0";
+        display.value = "0";
+    }
 }
 
-
+// Working on the delete button
 
 function deleteBtn() {
 
-    if(math_op[-1] === '(' && math_op[-2] === '*') {
-        math_op = math_op.slice(0, -2);
-    } else {
-        math_op = math_op.slice(0, -1);
+    if(isOn === false) {
+       return;
+    } else{
+
+        if(math_op[-1] === '(' && math_op[-2] === '*') {
+            math_op = math_op.slice(0, -2);
+        } else {
+            math_op = math_op.slice(0, -1);
+        }
+        display.value = math_op
+            .replace(/\*/g, '×')
+            .replace(/\//g, '÷');
+
+        if(math_op === '') {
+            display.value = '0';
+            math_op = "0"
+        }
     }
-    display.value = math_op
-        .replace(/\*/g, '×')
-        .replace(/\//g, '÷');
-        
-    /* let helper = math_op;
-    if(helper[-1] === '(' && helper[-2] === '*') {
-        helper = helper.slice(0, -2);
-        helper += '(';
-        display.value = helper;
-    }
-    helper = "";
-    */
 
 }
 
+
+// Working on the = button
 
 function calculate() {
-    try {
-        let result = '';
-        for(let i = 0; i < math_op.length; i++) {
-            if(math_op[i] === '÷'){
-                result += '/';
-            }
-            else if(math_op[i] === '^'){
-                result += '**';
-            }
-            else if(math_op[i] === 'π'){
-                result += Math.PI;
-            }
-            else if(math_op[i] === '²'){
-                result += '**2';
-            }
-            else if (math_op[i] === '×'){
-                result += '*';
-            }
-            else{
-                result += math_op[i];
-            }
-        }
 
-        display.value = eval(result);
-        math_op = display.value;
-        result = "";
-    }
-    catch {
-        display.value = "Error";
-        math_op = "";
-        result = '';
+    if(isOn === false) {
+       return;
+    } else {
+
+        try {
+            let result = '';
+            for(let i = 0; i < math_op.length; i++) {
+                if(math_op[i] === '÷'){
+                    result += '/';
+                }
+                else if(math_op[i] === '^'){
+                    result += '**';
+                }
+                else if(math_op[i] === 'π'){
+                    result += Math.PI;
+                }
+                else if(math_op[i] === '²'){
+                    result += '**2';
+                }
+                else if (math_op[i] === '×'){
+                    result += '*';
+                }
+                else{
+                    result += math_op[i];
+                }
+            }
+
+            display.value = eval(result);
+            math_op = display.value;
+            result = "";
+        }
+        catch {
+            display.value = "Error";
+            math_op = "";
+            result = '';
+        }
     }
 }
+
+
+
+
